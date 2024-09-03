fb39d0f76cffff8378f3131b11ab623d025e2d56
Jondiii <jondi@opendeusto.es>
2024-09-03 12:41:27 +0200

    script experimentos

---
+++ experimentation.py
@@ -0,0 +1,20 @@
+import subprocess
+import pandas as pd
+
+df_Experimentos = pd.read_csv('results_5Actions.csv', sep=';')
+listaExperimentos = list(df_Experimentos['run_name'])
+
+for experimento in listaExperimentos:
+
+    command = [
+        'python', 
+        'main.py', 
+        '--run_name', experimento, 
+        '--dir_data', 'data\\solomon_dataset\\C1', 
+        '--file_nodes', 'C101', 
+        '--file_vehicles', 'vehicles\\c1_vehicles',
+        '--iterations', '100',
+        '--timesteps', '102400'
+    ]
+
+    _ = subprocess.run(command, capture_output=True, text=True)
