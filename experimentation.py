d8ca367348ccd066fc5ce6618bde8a67daa7e4df
Jondiii <jondi@opendeusto.es>
2024-09-04 09:56:40 +0200

    ajustes para que funcione en el server

--- experimentation.py
+++ experimentation.py
@@ -10,11 +10,11 @@ for experimento in listaExperimentos:
         'python', 
         'main.py', 
         '--run_name', experimento, 
-        '--dir_data', 'data\\solomon_dataset\\C1', 
+        '--dir_data', 'data/solomon_dataset/C1',  # si no encuentra el path en local probar con \\
         '--file_nodes', 'C101', 
-        '--file_vehicles', 'vehicles\\c1_vehicles',
+        '--file_vehicles', 'vehicles/c1_vehicles',
         '--iterations', '100',
         '--timesteps', '102400'
     ]
 
-    _ = subprocess.run(command, capture_output=True, text=True)
+    print(subprocess.run(command, capture_output=True, text=True))
